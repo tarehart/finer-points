fc76d81dc2cbd4ea381240bd03a1ea9267d21970
Tyler Arehart <tarehart@gmail.com>
2016-01-20 22:52:26 -0800

    Using gulp to combine js and css files. Gradle will install gulp and its prerequisites on-demand.

---
+++ dev-environment-setup.md
@@ -0,0 +1,18 @@
+# Development Environment Setup Steps
+
+1. Install Neo4j Community Edition: http://neo4j.com/download/
+    - Launch the server, connect to localhost:7474, and choose an arbitrary password
+2. Create an application.properties file in this directory with values for:
+    - googleClientId=( look up here: https://console.developers.google.com/project/node-stand/apiui/credential )
+    - googleClientSecret=( lookup here: https://console.developers.google.com/project/node-stand/apiui/credential )
+    - neo4jUrl=http://localhost:7474
+    - neo4jUsername=neo4j
+    - neo4jPassword=( you'll be prompted to choose one when connecting to localhost:7474 )
+3. Create an Intellij project at the root directory
+4. On the command line, execute `./gradlew idea` which will configure your Intellij project with the right dependencies.
+
+
+## Rapid Iteration on JS / CSS
+
+1. Set gulp task runner in Intellij: https://www.jetbrains.com/idea/help/using-gulp-task-runner.html
+2. Open gulpfile.js, right click on the "watch" task and run it. This will keep your scripts and css up to date.
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+var root = './src/main/resources/static/';
+
+gulp.task('css-local', function() {
+    return gulp.src(root + 'css/local/**/*.css')
+        .pipe(concat('combined.css'))
+        .pipe(gulp.dest(root + 'dist/'));
+});
+
+// This provides a file useful for offline development, e.g. on an airplane
+gulp.task('css-cdn', function() {
+    return gulp.src(root + 'css/cdn/**/*.css')
+        .pipe(concat('combined-cdn.css'))
+        .pipe(gulp.dest(root + 'dist/'));
+});
+
+gulp.task('js-local', function() {
+    return gulp.src([
+        root + 'js/local/app.js',
+        root + 'js/local/showdown-node-stand.js',
+        root + 'js/local/**/*.js'])
+        .pipe(concat('combined.js'))
+        .pipe(gulp.dest(root + 'dist/'));
+});
+
+gulp.task('js-cdn', function() {
+    return gulp.src([
+        root + 'js/cdn/jquery*',
+        root + 'js/cdn/angular.min.js',
+        root + 'js/cdn/angular-route.min.js',
+        root + 'js/cdn/angular-sanitize.min.js',
+        root + 'js/cdn/bootstrap.min.js',
+        root + 'js/cdn/ui-bootstrap-tpls-0.14.3.min.js',
+        root + 'js/cdn/showdown.js',
+        root + 'js/cdn/**/*.js'])
+        .pipe(concat('combined-cdn.js'))
+        .pipe(gulp.dest(root + 'dist/'));
+});
+
+gulp.task('build', ['css-local', 'css-cdn', 'js-local', 'js-cdn']);
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch(root + 'css/local/**/*.css', ['css-local']);
+    gulp.watch(root + 'css/cdn/**/*.js', ['css-cdn']);
+    gulp.watch(root + 'js/local/**/*.js', ['js-local']);
+    gulp.watch(root + 'js/cdn/**/*.js', ['js-cdn']);
+});
