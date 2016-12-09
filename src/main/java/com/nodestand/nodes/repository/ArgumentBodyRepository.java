f4f804971d49674b000b3c7681b6dbee397c69bb
Tyler Arehart <tarehart@gmail.com>
2016-12-08 23:15:31 -0800

    Fixing node search and the drafts view.

--- src/main/java/com/nodestand/nodes/repository/ArgumentBodyRepository.java
+++ src/main/java/com/nodestand/nodes/repository/ArgumentBodyRepository.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public interface ArgumentBodyRepository extends GraphRepository<ArgumentBody> {
 
-    @Query("MATCH p=(a:User)<-[:AUTHORED_BY]-(n:ArgumentBody)-[:VERSION_OF]->(m:MajorVersion) WHERE n.title=~{0} AND (n.isPublic OR id(a)={1}) return p")
+    @Query("MATCH p=(u:User)<-[CONTROLLED_BY]-(:Author)<-[:AUTHORED_BY]-(n:ArgumentBody)-[:VERSION_OF]->(m:MajorVersion) WHERE n.title=~{0} AND (n.isPublic OR id(u)={1}) return p")
     Set<ArgumentBody> queryTitlesRich(String query, long searcherId);
 
 }
