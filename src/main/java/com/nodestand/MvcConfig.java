db4e226724e9e79ccedb818a3af4a6703e4c8588
Tyler Arehart <tarehart@gmail.com>
2016-12-12 22:22:35 -0800

    Upgrading gradle, webpack, spring boot. Rendering index.html on 404.

--- src/main/java/com/nodestand/MvcConfig.java
+++ src/main/java/com/nodestand/MvcConfig.java
@@ -1,8 +1,14 @@
 package com.nodestand;
 
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
@@ -10,6 +16,13 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    ErrorViewResolver supportPathBasedLocationStrategyWithoutHashes() {
+        return (request, status, model) ->
+                status == HttpStatus.NOT_FOUND ?
+                        new ModelAndView("index", Collections.emptyMap(), HttpStatus.OK) : null;
     }
 
 }
--- src/main/java/com/nodestand/nodes/repository/ArgumentNodeRepository.java
+++ src/main/java/com/nodestand/nodes/repository/ArgumentNodeRepository.java
@@ -11,8 +11,8 @@ import java.util.Set;
 
 public interface ArgumentNodeRepository extends GraphRepository<ArgumentNode> {
 
-    @Query("START t=node({0}), r=node({1}) MATCH p=t<-[:SUPPORTED_BY|INTERPRETS*0..]-r " +
-            "WITH p as p, nodes(p) as path UNWIND path AS item MATCH mvPath=item-[:DEFINED_BY]->(:ArgumentBody)-[:VERSION_OF]->(:MajorVersion) " +
+    @Query("START t=node({0}), r=node({1}) MATCH p=(t)<-[:SUPPORTED_BY|INTERPRETS*0..]-(r) " +
+            "WITH p as p, nodes(p) as path UNWIND path AS item MATCH mvPath=(item)-[:DEFINED_BY]->(:ArgumentBody)-[:VERSION_OF]->(:MajorVersion) " +
             "RETURN path, rels(p), nodes(mvPath), rels(mvPath)")
     Result getPaths(long childId, long rootId);
 
@@ -20,7 +20,7 @@ public interface ArgumentNodeRepository extends GraphRepository<ArgumentNode> {
             "-[:DEFINED_BY]->(body:ArgumentBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return path")
     Set<ArgumentNode> getGraph(String stableRootId);
 
-    @Query("start mv=node({0}) match p=mv<-[:VERSION_OF]-(:ArgumentBody)" +
+    @Query("start mv=node({0}) match p=(mv)<-[:VERSION_OF]-(:ArgumentBody)" +
             "<-[:DEFINED_BY]-(node:ArgumentNode) return p")
     Set<ArgumentNode> getNodesInMajorVersion(long majorVersionId);
 
@@ -32,43 +32,43 @@ public interface ArgumentNodeRepository extends GraphRepository<ArgumentNode> {
 
     @Query("match p=(n:AssertionNode)-[:DEFINED_BY]->(b:ArgumentBody {isPublic:true})-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author)" +
             " where not (:AssertionNode)-[:SUPPORTED_BY]->(n)" +
-            " with p as p, b as b match q=b-[:AUTHORED_BY]->(:Author) return p, q")
+            " with p as p, b as b match q=(b)-[:AUTHORED_BY]->(:Author) return p, q")
     Set<ArgumentNode> getRootNodesRich();
 
     @Query("match p=(n:ArgumentNode {stableId: {0}})-[:DEFINED_BY]->(b:ArgumentBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author)" +
-            " with p as p, b as b match q=b-[:AUTHORED_BY]->(:Author) return p, q")
+            " with p as p, b as b match q=(b)-[:AUTHORED_BY]->(:Author) return p, q")
     ArgumentNode getNodeRich(String stableId);
 
-    @Query("start n=node({0}) match body-[VERSION_OF]->n return max(body.minorVersion)")
+    @Query("start n=node({0}) match (body)-[VERSION_OF]->(n) return max(body.minorVersion)")
     Integer getMaxMinorVersion(long majorVersionId);
 
 
-    @Query("start n=node({0}) match node-[DEFINED_BY]->n return max(node.buildVersion)")
+    @Query("start n=node({0}) match (node)-[DEFINED_BY]->(n) return max(node.buildVersion)")
     Integer getMaxBuildVersion(long bodyId);
 
-    @Query("start n=node({0}) match n-[:SUPPORTED_BY*0..]->(support:ArgumentNode) " +
-            "WHERE NOT support-[:INTERPRETS]->(:SourceNode) AND NOT support-[:SUPPORTED_BY]->(:ArgumentNode) return support")
+    @Query("start n=node({0}) match (n)-[:SUPPORTED_BY*0..]->(support:ArgumentNode) " +
+            "WHERE NOT (support)-[:INTERPRETS]->(:SourceNode) AND NOT (support)-[:SUPPORTED_BY]->(:ArgumentNode) return support")
     Set<ArgumentNode> getUnsupportedNodes(long nodeId);
 
-    @Query("start n=node({0}) match p=n-[:DEFINED_BY]->(:ArgumentBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return p")
+    @Query("start n=node({0}) match p=(n)-[:DEFINED_BY]->(:ArgumentBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return p")
     ArgumentNode loadWithMajorVersion(long id);
 
     @Query("match p=(:Author {stableId: {0}})<-[:AUTHORED_BY]-(b:ArgumentBody)<-[:DEFINED_BY]-(n:ArgumentNode) where not b.isPublic" +
-            " with p as p, b as b match q=b-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return p, q")
+            " with p as p, b as b match q=(b)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return p, q")
     Set<ArgumentNode> getDraftNodesRich(String authorStableId);
 
     @Query("match p=(:Author {stableId: {0}})<-[:AUTHORED_BY]-(mv:MajorVersion)<-[:VERSION_OF]-(b:ArgumentBody)<-[:DEFINED_BY]-(n:ArgumentNode) where b.isPublic" +
-            " with p as p, b as b match q=b-[:AUTHORED_BY]->(:Author) return p, q")
+            " with p as p, b as b match q=(b)-[:AUTHORED_BY]->(:Author) return p, q")
     Set<ArgumentNode> getNodesOriginallyAuthoredByUser(String authorStableId);
 
-    @Query("start n=node({0}) match (c:ArgumentNode)-[:SUPPORTED_BY|INTERPRETS]->n" +
-            " with c match p=c-[:DEFINED_BY]->(b:ArgumentBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) where b.isPublic" +
-            " with p as p, b as b match q=b-[:AUTHORED_BY]->(:Author) return p, q")
+    @Query("start n=node({0}) match (c:ArgumentNode)-[:SUPPORTED_BY|INTERPRETS]->(n)" +
+            " with c match p=(c)-[:DEFINED_BY]->(b:ArgumentBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) where b.isPublic" +
+            " with p as p, b as b match q=(b)-[:AUTHORED_BY]->(:Author) return p, q")
     Set<ArgumentNode> getConsumerNodes(long nodeId);
 
-    @Query("MATCH (c:ArgumentNode)-[:SUPPORTED_BY|INTERPRETS]->n WHERE ID(n) = {0}" +
-            " with c match p=c-[:DEFINED_BY]->(b:ArgumentBody)-[:AUTHORED_BY]->(:Author)-[:CONTROLLED_BY]->(u:User) where b.isPublic OR ID(u) = {1}" +
-            " with p as p, b as b match q=b-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return p, q")
+    @Query("MATCH (c:ArgumentNode)-[:SUPPORTED_BY|INTERPRETS]->(n) WHERE ID(n) = {0}" +
+            " with c match p=(c)-[:DEFINED_BY]->(b:ArgumentBody)-[:AUTHORED_BY]->(:Author)-[:CONTROLLED_BY]->(u:User) where b.isPublic OR ID(u) = {1}" +
+            " with p as p, b as b match q=(b)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author) return p, q")
     Set<ArgumentNode> getConsumerNodes(long nodeId, long userId);
 
     /**
@@ -85,7 +85,7 @@ public interface ArgumentNodeRepository extends GraphRepository<ArgumentNode> {
      */
     @Query("MATCH p=(rootNode:ArgumentNode {stableId:{0}})-[:DEFINED_BY]->(rootBody:ArgumentBody)-[:PRECEDED_BY*0..]->(:ArgumentBody)" +
             " WITH p as p, nodes(p) as pathNodes, rootBody as rootBody, rootNode as rootNode" +
-            " MATCH mvPath=rootBody-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author)" +
+            " MATCH mvPath=(rootBody)-[:VERSION_OF]->(:MajorVersion)-[:AUTHORED_BY]->(:Author)" +
             " UNWIND pathNodes AS bdy" +
             " MATCH editorPath=(bdy:ArgumentBody)-[:AUTHORED_BY]->(editor:Author)" +
             " MATCH argumentPath=(bdy:ArgumentBody)<-[:DEFINED_BY]-(argNode:ArgumentNode)" +
