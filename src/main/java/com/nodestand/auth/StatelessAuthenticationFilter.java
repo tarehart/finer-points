c5fdb5419a1e46a955e30e7daeeabe78d60647e4
Tyler Arehart <tarehart@gmail.com>
2017-01-03 14:33:18 -0500

    DO NOT DEPLOY: making the authentication filter make / lookup users according to user agent string. Multi-user testing with no internet access.

--- src/main/java/com/nodestand/auth/StatelessAuthenticationFilter.java
+++ src/main/java/com/nodestand/auth/StatelessAuthenticationFilter.java
@@ -1,6 +1,8 @@
 package com.nodestand.auth;
 
 import com.nodestand.controllers.ResourceNotFoundException;
+import com.nodestand.nodes.User;
+import com.nodestand.nodes.repository.UserRepository;
 import com.nodestand.service.user.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,6 +20,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 public class StatelessAuthenticationFilter extends GenericFilterBean {
@@ -28,12 +31,15 @@ public class StatelessAuthenticationFilter extends GenericFilterBean {
 
     private final UserService userService;
 
+    private final UserRepository userRepository;
+
     private final Log logger = LogFactory.getLog(getClass());
 
     @Autowired
-    public StatelessAuthenticationFilter(TokenHandler tokenHandler, UserService userService) {
+    public StatelessAuthenticationFilter(TokenHandler tokenHandler, UserService userService, UserRepository userRepository) {
         this.tokenHandler = tokenHandler;
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -41,6 +47,55 @@ public class StatelessAuthenticationFilter extends GenericFilterBean {
 			ServletException {
 
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        //jwtFilter(httpRequest);
+        userAgentTestFilter(httpRequest);
+
+		chain.doFilter(request, response);
+	}
+
+	private void userAgentTestFilter(HttpServletRequest httpRequest) {
+
+        String userAgent = httpRequest.getHeader("User-Agent");
+
+        String providerId = "userAgent";
+        String providerUserId = userAgent;
+
+        Optional<NodeUserDetails> userDetails = userService.loadUserBySocialProvider(providerId, providerUserId);
+
+        NodeUserDetails concreteUser;
+
+        // Step 3b. Create a new user account or return an existing one.
+        if (userDetails.isPresent()) {
+            concreteUser = userDetails.get();
+        } else {
+
+            //add new users to the db with its default roles for later use in SocialAuthenticationSuccessHandler
+            final User user = new User(
+                    providerId,
+                    providerUserId,
+                    User.Roles.ROLE_USER);
+
+            String[] userAgentParts = userAgent.split(" ");
+
+            // Start with three aliases
+            user.addNewAlias(userAgentParts[userAgentParts.length - 1]);
+
+            userRepository.save(user);
+
+            concreteUser = new NodeUserDetails(user);
+        }
+
+        setUserInContext(httpRequest, concreteUser);
+    }
+
+    private void setUserInContext(HttpServletRequest httpRequest, NodeUserDetails concreteUser) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(concreteUser, null, concreteUser.getAuthorities());
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void jwtFilter(HttpServletRequest httpRequest) {
         String authToken = getToken(httpRequest);
         String stableId = tokenHandler.getUserStableIdFromToken(authToken);
 
@@ -51,17 +106,13 @@ public class StatelessAuthenticationFilter extends GenericFilterBean {
                 NodeUserDetails userDetails = this.userService.loadUserByUserId(stableId);
 
                 if (tokenHandler.validateToken(authToken, userDetails)) {
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    setUserInContext(httpRequest, userDetails);
                 }
             } catch (ResourceNotFoundException e) {
                 logger.error("Failed attempt to log in because matching user not found in database.", e);
             }
         }
-
-		chain.doFilter(request, response);
-	}
+    }
 
     private String getToken(HttpServletRequest httpRequest) {
         String authHeader = httpRequest.getHeader(TOKEN_HEADER_NAME);
