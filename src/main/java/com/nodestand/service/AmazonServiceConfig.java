dbca15fdcaa1a87493cebdd2eda8c24f855b513a
Tyler Arehart <tarehart@gmail.com>
2017-01-28 09:27:11 -0800

    Adding dependency on simple email service client. Putting AWS clients in single spring config file.

---
+++ src/main/java/com/nodestand/service/AmazonServiceConfig.java
@@ -0,0 +1,39 @@
+package com.nodestand.service;
+
+import com.amazonaws.services.logs.AWSLogs;
+import com.amazonaws.services.logs.AWSLogsClient;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class AmazonServiceConfig {
+
+    @Autowired
+    public AmazonServiceConfig(Environment environment) {
+        // In some cases it's OK if these are null.
+        // I may decide not to provide them and let the AWS SDK fall back
+        // to the EC2 role (if we're currently running on EC2).
+        // If you're running a dev environment you'll need these though.
+        String accessKeyId = environment.getProperty("aws.accessKeyId");
+        String secretKey = environment.getProperty("aws.secretKey");
+        if (accessKeyId != null && secretKey != null) {
+            System.setProperty("aws.accessKeyId", accessKeyId);
+            System.setProperty("aws.secretKey", secretKey);
+        }
+    }
+
+    @Bean
+    public AWSLogs awsLogs() {
+        return new AWSLogsClient();
+    }
+
+    @Bean
+    public AmazonSimpleEmailService simpleEmailService() {
+        return new AmazonSimpleEmailServiceClient();
+    }
+
+}
--- src/main/java/com/nodestand/service/vote/ScoreLogReader.java
+++ src/main/java/com/nodestand/service/vote/ScoreLogReader.java
@@ -1,12 +1,10 @@
 package com.nodestand.service.vote;
 
-import com.amazonaws.services.logs.AWSLogsClient;
+import com.amazonaws.services.logs.AWSLogs;
 import com.amazonaws.services.logs.model.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -18,24 +16,12 @@ import java.util.stream.Collectors;
 @Component
 public class ScoreLogReader {
 
-    private AWSLogsClient awsLogsClient;
-    private ObjectMapper objectMapper;
+    private final AWSLogs awsLogs;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public ScoreLogReader(Environment environment) {
-
-        // In some cases it's OK if these are null.
-        // I may decide not to provide them and let the AWS SDK fall back
-        // to the EC2 role (if we're currently running on EC2).
-        // If you're running a dev environment you'll need these though.
-        String accessKeyId = environment.getProperty("aws.accessKeyId");
-        String secretKey = environment.getProperty("aws.secretKey");
-        if (accessKeyId != null && secretKey != null) {
-            System.setProperty("aws.accessKeyId", accessKeyId);
-            System.setProperty("aws.secretKey", secretKey);
-        }
-
-        awsLogsClient = new AWSLogsClient();
+    public ScoreLogReader(AWSLogs awsLogs) {
+        this.awsLogs = awsLogs;
         objectMapper = new ObjectMapper();
     }
 
@@ -53,7 +39,7 @@ public class ScoreLogReader {
         String token = null;
         do {
             request.setNextToken(token);
-            FilterLogEventsResult filterLogEventsResult = awsLogsClient.filterLogEvents(request);
+            FilterLogEventsResult filterLogEventsResult = awsLogs.filterLogEvents(request);
             scoreLogs.addAll(getLogs(filterLogEventsResult));
             token = filterLogEventsResult.getNextToken();
         } while (token != null);
